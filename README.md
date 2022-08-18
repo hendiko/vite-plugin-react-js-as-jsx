fe6e9a02b0a5b3b3039606437ccbdef4888529ec
Xavier Yin <hendikoy@gmail.com>
2022-08-18 15:18:30 +0800

    扩展 options 可选项，支持 rule, include, exclude, ignoreNodeModules，更改包名为 vite-plugin-react-js-as-jsx

--- README.md
+++ README.md
@@ -1,18 +1,56 @@
-# vite-plugin-react-js-support
-vite对jsx语法只认tsx和jsx后缀的文件，可是项目中有很多js类型的文件也是jsx；
+> 本项目 fork 自 [dravenww/vite-plugin-react-js-support](https://github.com/dravenww/vite-plugin-react-js-support)
+
+# vite-plugin-react-js-as-jsx
+
+vite 对 jsx 语法只认 tsx 和 jsx 后缀的文件，可是项目中有很多 js 类型的文件也是 jsx；
+
+# API
+
+```js
+import vitePluginReactJsAsJsx from "vite-plugin-react-js-as-jsx";
+
+const plugin = vitePluginReactJsAsJsx(babelPlugins, options);
+```
+
+- babelPlguins:`Array` Babel 的插件列表。
+- options
+  - jsxInject: `Boolean` 是否注入 "import React from 'react'", 默认为 `true`。
+  - rule: `Regexp|Function|String` 探测文件是否需要本插件进行处理。
+  - include: `Array<rule>` 需要包含在内的规则数组。
+  - exclude: `Array<rule>` 需要排除在外的规则数组。
+  - ignoreNodeModules: `Boolean` 是否排除 `node_modules` 目录。默认为 `true`。
+
+在某些情况下，你可能需要处理从第三方导入的 '.js'，可以在 'include' 中添加规则，'include' 规则高于 'exclude'。
+
+示例：
+
+```js
+import vitePlugin from "vite-plugin-react-js-as-jsx";
+export default {
+  plugins: [
+    vitePlugin([], {
+      jsxInject: true,
+      include: ["node_module/some-third-party-lib"], // 表示some-third-party-lib 这个包下的代码需要本插件处理
+    }),
+  ],
+};
+```
 
 # 安装
+
 ```
 
-npm install --save-dev vite-plugin-react-js-support
+npm install --save-dev vite-plugin-react-js-as-jsx
 or
-yarn add vite-plugin-react-js-support
+yarn add vite-plugin-react-js-as-jsx
 
 
 ```
+
 # 使用
+
 ```
-import vitePlugin from 'vite-plugin-react-js-support';
+import vitePlugin from 'vite-plugin-react-js-as-jsx';
 export default {
     plugins: [
         vitePlugin([], { jsxInject: true }),
@@ -20,7 +58,9 @@ export default {
 }
 
 ```
+
 # 备注
+
 ```
 // 对于vite预构建会报错的问题，需要把配置改下
 export default ({command, mode}) => {
@@ -40,6 +80,7 @@ export default ({command, mode}) => {
 ```
 
 # 参数
+
 ```
 // 之所以支持这个配置，是因为cra4版本以后在子文件中不会引入react；
 // 如果你的子文件已经引入了react，可以把这项设置为false;
@@ -48,4 +89,4 @@ export default ({command, mode}) => {
 jsxInject: true, // 是否为js文件注入 import React from 'react';默认为true
 ```
 
-还支持传入其他babel的plugin，数组类型。
+还支持传入其他 babel 的 plugin，数组类型。
--- src/index.js
+++ src/index.js
@@ -1,41 +1,84 @@
-import babelCore from '@babel/core';
+/*
+ * @Author: Xavier Yin
+ * @Date: 2022-08-18 11:36:32
+ * @Description:
+ */
+import babelCore from "@babel/core";
 const cwd = process.cwd();
-const fileRegex = /\.js$/
 // 获取react版本
-const version = require(cwd + '/node_modules/react/package.json').version;
-const greatThan17 = parseInt(version.split('.')[0]) >= 17;
+const version = require(cwd + "/node_modules/react/package.json").version;
+const greatThan17 = parseInt(version.split(".")[0]) >= 17;
 
-function vitePluginReactJsSupport(plugins, options) {
-  options = options ? options: {
-    jsxInject: true,
-  };
-  const jsxInject = options.jsxInject === undefined ? true : options.jsxInject;
-  return {
-    name: 'vite-plugin-react-js-support',
-    transform(source, id) {
-      if (fileRegex.test(id) && id.indexOf('node_modules') === -1) {
-        if (greatThan17 && jsxInject &&
-          source.indexOf('import React') === -1
-          && source.indexOf('from "react"') === -1
-          && source.indexOf("from 'react'") === -1)
-        {
-          source = 'import React from "react"; \n' + source
-        }
-        const ps = Array.isArray(plugins) ? plugins : [];
-        return {
-          code: compileFileToJS(source, ps),
-          map: null // provide source map if available
-        }
-      }
-    }
+const isRegexp = (any) => any instanceof RegExp;
+const isFunction = (any) => typeof any === "function";
+
+const makeMatch = (rule) =>
+  isRegexp(rule)
+    ? (str) => rule.test(str)
+    : isFunction(rule)
+    ? (str) => !!rule(str)
+    : (str) => str.indexOf(rule) >= 0;
+
+/**
+ *
+ * @param {array} babelPlugins
+ * @param {object} options
+ * - jsxInject {boolean}: inject statement "import React from 'react'" or not. Default is true.
+ * - match {regexp|function|string}: determine file if it should be processed by this plugin.
+ * - include: {Array<regexp|string>}: the rules to tell which file needs to be processed. This is prior to exclude rules.
+ * - exclude: {Array<regexp|string>}: the rules to tell which file should not be processed.
+ * @returns
+ */
+function vitePluginReactJsAsJsx(babelPlugins, options) {
+  const {
+    jsxInject = true,
+    rule = /\.(js|ts)$/,
+    include = [],
+    exclude = [],
+    ignoreNodeModules = true, // default to ignore node_modules directory
+  } = options || {};
+
+  const canMatch = makeMatch(rule);
+
+  const includes = (include || []).map((item) => makeMatch(item));
+  const excludes = (exclude || []).map((item) => makeMatch(item));
+
+  if (ignoreNodeModules) {
+    excludes.unshift(makeMatch("node_modules"));
   }
+
+  // include rules are prior to exclude rules
+  const canInclude = (str) =>
+    includes.some((fn) => fn(str)) || !excludes.some((fn) => fn(str));
+
+  return {
+    name: "vite-plugin-react-js-as-jsx",
+    transform(source, id) {
+      if (canMatch(id) && canInclude(id)) {
+        if (
+          greatThan17 &&
+          jsxInject &&
+          source.indexOf("import React") === -1 &&
+          source.indexOf('from "react"') === -1 &&
+          source.indexOf("from 'react'") === -1
+        ) {
+          source = 'import React from "react"; \n' + source;
+        }
+        const plugins = Array.isArray(babelPlugins) ? babelPlugins : [];
+        return {
+          code: compileFileToJS(source, plugins),
+          map: null, // provide source map if available
+        };
+      }
+    },
+  };
 }
 
 function compileFileToJS(source, plugins) {
   const result = babelCore.transformSync(source, {
     plugins: ["@babel/plugin-transform-react-jsx", ...plugins],
-  })
+  });
   return result.code;
 }
 
-module.exports = vitePluginReactJsSupport
+module.exports = vitePluginReactJsAsJsx;
